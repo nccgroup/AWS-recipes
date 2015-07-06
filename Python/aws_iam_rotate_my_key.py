e64720de74beebfcbba8945b6571337ffa90810b
l01c d3v <l01cd3v@gmail.com>
2015-07-05 22:18:31 -0700

    Migrated init session and rotate my key

--- Python/aws_iam_rotate_my_key.py
+++ Python/aws_iam_rotate_my_key.py
@@ -6,7 +6,6 @@ from AWSUtils.utils_iam import *
 from AWSUtils.utils_sts import *
 
 # Import third-party modules
-import boto
 import shutil
 import sys
 import traceback
@@ -28,7 +27,7 @@ def main(args):
     try:
         print 'Connecting to AWS IAM...'
         session_key_id, session_secret, mfa_serial, session_token = read_creds_from_aws_credentials_file(profile_name)
-        iam_connection = boto.connect_iam(aws_access_key_id = session_key_id, aws_secret_access_key = session_secret, security_token = session_token)
+        iam_client = connect_iam(session_key_id, session_secret, session_token)
     except Exception, e:
         printException(e)
         return 42
@@ -41,7 +40,7 @@ def main(args):
 
     # Fetch current user name
     if not user_name:
-        user_name = fetch_from_current_user(iam_connection, aws_key_id, 'user_name')
+        user_name = fetch_from_current_user(iam_client, aws_key_id, 'UserName')
         if not user_name:
             print 'Error'
             return 42
@@ -50,10 +49,10 @@ def main(args):
     try:
         # Create a new IAM key
         print 'Creating a new access key for \'%s\'...' % user_name
-        new_key = iam_connection.create_access_key(user_name)
-        new_key_id = new_key['create_access_key_response']['create_access_key_result']['access_key']['access_key_id']
-        new_secret = new_key['create_access_key_response']['create_access_key_result']['access_key']['secret_access_key']
-        list_access_keys(iam_connection, user_name)
+        new_key = iam_client.create_access_key(UserName = user_name)
+        new_key_id = new_key['AccessKey']['AccessKeyId']
+        new_secret = new_key['AccessKey']['SecretAccessKey']
+        list_access_keys(iam_client, user_name)
     except Exception, e:
         printException(e)
         return 42
@@ -74,10 +73,10 @@ def main(args):
     try:
         print 'Verifying access with the new key...'
         session_key_id, session_secret, mfa_serial, session_token = read_creds_from_aws_credentials_file(profile_name)
-        new_connection = boto.connect_iam(aws_access_key_id = session_key_id, aws_secret_access_key = session_secret, security_token = session_token)
+        new_iam_client = connect_iam(session_key_id, session_secret, session_token)
         print 'Deleting the old access key...'
-        new_connection.delete_access_key(aws_key_id, user_name)
-        list_access_keys(iam_connection, user_name)
+        new_iam_client.delete_access_key(AccessKeyId = aws_key_id, UserName = user_name)
+        list_access_keys(iam_client, user_name)
     except Exception, e:
         printException(e)
         return 42
