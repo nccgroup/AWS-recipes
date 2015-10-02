bae37024133f16cb620de729d5d430e7ed1b7ebe
l01c d3v <l01cd3v@gmail.com>
2015-10-01 19:23:45 -0700

    Add support for interactive an CSV modes in create_ip_ranges Add unit tests for that recipe

--- Python/aws_recipes_create_ip_ranges.py
+++ Python/aws_recipes_create_ip_ranges.py
@@ -20,16 +20,11 @@ def new_ip_info(region, instance_id, is_elastic):
     ip_info['is_elastic'] = is_elastic
     return ip_info
 
-def new_prefix(ip_prefix, obj): 
+def new_prefix(ip_prefix, obj):
     obj['ip_prefix'] = ip_prefix
     return obj
 
-def save_ip_ranges(profile_name, prefixes, force_write, debug):
-    filename = 'ip-ranges-%s.json' % profile_name
-    ip_ranges = {}
-    ip_ranges['createDate'] = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    ip_ranges['prefixes'] = prefixes
-    save_blob_as_json(filename, ip_ranges, force_write, debug)
+
 
 
 ########################################
@@ -42,7 +37,7 @@ def main(args):
     configPrintException(args.debug)
 
     # Check version of opinel
-    if not check_opinel_version('0.17.0'):
+    if not check_opinel_version('0.18.0'):
         return 42
 
     # Get the environment name
@@ -54,49 +49,125 @@ def main(args):
     # For each profile/environment...
     for profile_name in profile_names:
 
-        # Initialize IP addresses
-        printInfo('Fetching public IP information for the \'%s\' environment...' % profile_name)
-        ip_addresses = {}
+        # Interactive mode
+        if args.interactive:
 
-        # Search for AWS credentials
-        key_id, secret, session_token = read_creds(profile_name)
-        if not key_id:
-            return 42
+            # Initalize prefixes
+            attributes = args.attributes
+            filename = 'ip-ranges-%s.json' % profile_name
+            if os.path.isfile(filename):
+                printInfo('Loading existing IP ranges from %s' % filename)
+                prefixes = read_ip_ranges(filename)
+                # Initialize attributes from existing values
+                if attributes == []:
+                    for prefix in prefixes:
+                        for key in prefix:
+                            if key not in attributes:
+                                attributes.append(key)
+            else:
+                prefixes = []
 
-        # For each region...
-        for region in regions:
+            # IP prefix does not need to be specified as an attribute
+            attributes = [a for a in attributes if a != 'ip_prefix']
 
-            # Connect to EC2
-            ec2_client = connect_ec2(key_id, secret, session_token, region)
-            if not ec2_client:
-                continue
+            # Prompt for new entries
+            while prompt_4_yes_no('Add a new IP prefix to the ip ranges'):
+                ip_prefix = prompt_4_value('Enter the new IP prefix:')
+                obj = {}
+                for a in attributes:
+                    obj[a] = prompt_4_value('Enter the \'%s\' value:' % a)
+                prefixes.append(new_prefix(ip_prefix, obj))
 
-            # Get public IP addresses associated with EC2 instances
-            printInfo('...in %s : EC2 instances' % region)
-            reservations = handle_truncated_response(ec2_client.describe_instances, {}, 'NextToken', ['Reservations'])
-            for reservation in reservations['Reservations']:
-                for i in reservation['Instances']:
-                    if 'PublicIpAddress' in i:
-                        ip_addresses[i['PublicIpAddress']] = new_ip_info(region, i['InstanceId'], False) 
-                    if 'NetworkInterfaces' in i:
-                        for eni in i['NetworkInterfaces']:
-                            if 'Association' in eni:
-                                ip_addresses[eni['Association']['PublicIp']] = new_ip_info(region, i['InstanceId'], False) # At that point, we don't know whether it's an EIP or not...
+        # Support loading from CSV file
+        elif len(args.csv_ip_ranges) > 0:
 
-            # Get all EIPs (to handle unassigned cases)
-            printInfo('...in %s: Elastic IP addresses' % region)
-            eips = handle_truncated_response(ec2_client.describe_addresses, {}, 'NextToken', ['Addresses'])
-            for eip in eips['Addresses']:
-                instance_id = eip['InstanceId'] if 'InstanceId' in eip else None
-                # EC2-Classic non associated EIPs have an empty string for instance ID (instead of lacking the attribute in VPC) 
-                if instance_id == '':
-                    instance_id = None
-                ip_addresses[eip['PublicIp']] = new_ip_info(region, instance_id, True)
-
-            # Format 
+            # Initalize prefixes
             prefixes = []
-            for ip in ip_addresses:
-                prefixes.append(new_prefix(ip, ip_addresses[ip]))
+
+            # Load CSV file contents
+            for filename in args.csv_ip_ranges:
+                with open(filename, 'rt') as f:
+                    csv_contents = f.readlines()
+
+                # Initialize mappings
+                attributes = args.attributes
+                mappings = {}
+                if attributes == []:
+                    # Follow structure of first line
+                    headers = csv_contents.pop(0).strip().split(',')
+                    for index, attribute in enumerate(headers):
+                        mappings[attribute] = index
+                elif attributes and args.mappings == []:
+                    # Follow structure of first line but only map a subset of fields
+                    headers = csv_contents.pop(0).strip().split(',')
+                    attributes.append('ip_prefix')
+                    for attribute in set(attributes):
+                        mappings[attribute] = headers.index(attribute)
+                else:
+                    # Indices of columns are provided as an argument
+                    for index, attribute in enumerate(attributes):
+                        mappings[attribute] = int(args.mappings[index])
+                    if args.skip_first_line:
+                        csv_contents.pop(0)
+
+                # For each line...
+                for line in csv_contents:
+                    ip_prefix = {}
+                    values = line.strip().split(',')
+                    for attribute in mappings:
+                        ip_prefix[attribute] = values[mappings[attribute]]
+                    if 'ip_prefix' in mappings and 'mask' in mappings:
+                        ip = ip_prefix.pop('ip_prefix')
+                        mask = ip_prefix.pop('mask')
+                        ip_prefix['ip_prefix'] = '%s/%s' % (ip, mask.replace('/',''))
+                    prefixes.append(ip_prefix)
+
+        # AWS mode
+        else:
+
+            # Initialize IP addresses
+            printInfo('Fetching public IP information for the \'%s\' environment...' % profile_name)
+            ip_addresses = {}
+
+            # Search for AWS credentials
+            key_id, secret, session_token = read_creds(profile_name)
+            if not key_id:
+                return 42
+
+            # For each region...
+            for region in regions:
+
+                # Connect to EC2
+                ec2_client = connect_ec2(key_id, secret, session_token, region)
+                if not ec2_client:
+                    continue
+
+                # Get public IP addresses associated with EC2 instances
+                printInfo('...in %s : EC2 instances' % region)
+                reservations = handle_truncated_response(ec2_client.describe_instances, {}, 'NextToken', ['Reservations'])
+                for reservation in reservations['Reservations']:
+                    for i in reservation['Instances']:
+                        if 'PublicIpAddress' in i:
+                            ip_addresses[i['PublicIpAddress']] = new_ip_info(region, i['InstanceId'], False)
+                        if 'NetworkInterfaces' in i:
+                            for eni in i['NetworkInterfaces']:
+                                if 'Association' in eni:
+                                    ip_addresses[eni['Association']['PublicIp']] = new_ip_info(region, i['InstanceId'], False) # At that point, we don't know whether it's an EIP or not...
+
+                # Get all EIPs (to handle unassigned cases)
+                printInfo('...in %s: Elastic IP addresses' % region)
+                eips = handle_truncated_response(ec2_client.describe_addresses, {}, 'NextToken', ['Addresses'])
+                for eip in eips['Addresses']:
+                    instance_id = eip['InstanceId'] if 'InstanceId' in eip else None
+                    # EC2-Classic non associated EIPs have an empty string for instance ID (instead of lacking the attribute in VPC)
+                    if instance_id == '':
+                        instance_id = None
+                    ip_addresses[eip['PublicIp']] = new_ip_info(region, instance_id, True)
+
+                # Format
+                prefixes = []
+                for ip in ip_addresses:
+                    prefixes.append(new_prefix(ip, ip_addresses[ip]))
 
         # Generate an ip-ranges-<profile>.json file
         save_ip_ranges(profile_name, prefixes, args.force_write, args.debug)
@@ -113,6 +184,32 @@ add_common_argument(parser, default_args, 'with-gov')
 add_common_argument(parser, default_args, 'with-cn')
 add_common_argument(parser, default_args, 'force')
 add_common_argument(parser, default_args, 'dry-run')
+
+parser.add_argument('--interactive',
+                    dest='interactive',
+                    default=False,
+                    action='store_true',
+                    help='Interactive prompt to manually enter CIDRs.')
+parser.add_argument('--csv-ip-ranges',
+                    dest='csv_ip_ranges',
+                    default=[],
+                    nargs='+',
+                    help='CSV file(s) containing CIDRs information.')
+parser.add_argument('--skip-first-line',
+                    dest='skip_first_line',
+                    default=False,
+                    action='store_true',
+                    help='Skip first line when parsing CSV file.')
+parser.add_argument('--attributes',
+                    dest='attributes',
+                    default=[],
+                    nargs='+',
+                    help='Name of the attributes to enter for each CIDR.')
+parser.add_argument('--mappings',
+                    dest='mappings',
+                    default=[],
+                    nargs='+',
+                    help='Column number matching attributes when headers differ.')
 
 args = parser.parse_args()
 
--- tests/Python.py
+++ tests/Python.py
@@ -1,8 +1,12 @@
+# Import opinel
+from opinel.utils import *
+
+# Import stock packages
 import os
 from subprocess import Popen, PIPE
 
 #
-# Python AWS recipes test class
+# AWS recipes test class (Python only)
 #
 class TestPythonRecipesClass:
 
@@ -12,6 +16,7 @@ class TestPythonRecipesClass:
     def setUp(self):
         self.recipes_dir = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../Python'))
         self.recipes = [f for f in os.listdir(self.recipes_dir) if os.path.isfile(os.path.join(self.recipes_dir, f)) and f.endswith('.py')]
+        self.data_dir = 'tests/data'
 
     #
     # Every Python recipe must run fine with --help
@@ -28,4 +33,35 @@ class TestPythonRecipesClass:
                 successful_help_runs = False
         assert successful_help_runs
 
-    
+
+    #
+    # Tests for aws_recipes_create_ip_ranges.py
+    #
+    def test_aws_recipes_create_ip_ranges(self):
+        successful_aws_recipes_create_ip_ranges_runs = True
+        recipe = os.path.join(self.recipes_dir, 'aws_recipes_create_ip_ranges.py')
+        test_cases = [
+            # Matching header names, use all data
+            ['--csv-ip-ranges tests/data/ip-ranges-1.csv --force', 'ip-ranges-1a.json'],
+            # Matching header names, use partial data
+            ['--csv-ip-ranges tests/data/ip-ranges-1.csv --force --attributes ip_prefix field_b --skip-first-line', 'ip-ranges-1b.json'],
+            # Matching header names, use partial data with mappings (must skip first line)
+            ['--csv-ip-ranges tests/data/ip-ranges-1.csv --force --attributes ip_prefix field_b --mappings 0 2 --skip-first-line', 'ip-ranges-1c.json'],
+            # Matching header names but swap with mappings (must skip first line)
+            ['--csv-ip-ranges tests/data/ip-ranges-1.csv --force --attributes ip_prefix field_a --mappings 0 2 --skip-first-line', 'ip-ranges-1d.json'],
+            # No headers, use all data
+            ['--csv-ip-ranges tests/data/ip-ranges-2.csv --force --attributes ip_prefix field_a, field_b --mappings 0 1 2', 'ip-ranges-2a.json'],
+            # No headers, use partial data
+            ['--csv-ip-ranges tests/data/ip-ranges-2.csv --force --attributes ip_prefix field_b --mappings 0 2', 'ip-ranges-2b.json'],
+            # Different header names (must skip first line)
+            ['--csv-ip-ranges tests/data/ip-ranges-3.csv --force --attributes ip_prefix new_field_b --mappings 0 2 --skip-first-line', 'ip-ranges-3a.json'],
+            # Different header names with ip_prefix not in first column (must skip first line)
+            ['--csv-ip-ranges tests/data/ip-ranges-4.csv --force --attributes ip_prefix new_field_a new_field_b --mappings 1 0 2 --skip-first-line', 'ip-ranges-4a.json'],
+        ]
+        for test_case in test_cases:
+            args, result_file = test_case
+            cmd =  ['python' , recipe] + args.split(' ')
+            process = Popen(cmd, stdout=PIPE)
+            test_results = read_ip_ranges('ip-ranges-default.json')
+            known_results = read_ip_ranges(os.path.join(self.data_dir, result_file))
+            assert(cmp(test_results, known_results) == 0)
